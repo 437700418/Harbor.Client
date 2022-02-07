0a8362f186e6e87f26e4273da4568d0e4db58778
437700418 <437700418@qq.com>
2022-02-07 17:01:17 +0800

    add

---
+++ Harbor.Client/HarborClientConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using Harbor.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harbor.Client
+{
+    /// <summary>
+    /// Harbor访问配置
+    /// </summary>
+    public class HarborClientConfiguration
+    {
+        /// <summary>
+        /// Api服务的基础路径
+        /// </summary>
+        public string BaseUri { get; }
+
+        /// <summary>
+        /// Harbor用户名
+        /// </summary>
+
+        public string UserName { get; }
+
+        /// <summary>
+        /// Harbor密码
+        /// </summary>
+        public string Password { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="harborConfig"></param>
+        public HarborClientConfiguration(HarborConfig harborConfig) : this(harborConfig.ServerAddress, harborConfig.Username, harborConfig.Password)
+        {
+
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="baseurl">HarborAPi基础地址</param>
+        /// <param name="username">Harbor用户名</param>
+        /// <param name="password">Harbor密码</param>
+        public HarborClientConfiguration(string baseurl, string username, string password)
+        {
+            BaseUri = baseurl == null ? "" : (baseurl.StartsWith("http://") ? baseurl : "http://" + baseurl);
+            UserName = username ?? "";
+            Password = password ?? "";
+        }
+
+        /// <summary>
+        /// 创建Harbor请求客户端
+        /// </summary>
+        /// <returns></returns>
+        public HarborClient CreatHarborClient()
+        {
+            return new HarborClient(this);
+        }
+    }
+}
